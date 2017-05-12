21c27d797dba9f01b995d15bf27d7020010d36da
João Lucas <joao.lucas.lucchetta@gmail.com>
2017-05-11 21:08:42 -0300

    parser: add `parse` and `parseLine`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-let foo = '123';
+const matchers = require('./matchers');
+
+const parseLine = (line) => {
+  const result = {};
+  Object
+    .entries(matchers)
+    .forEach(([name, matcher]) => {
+      result[name] = matcher(line);
+    });
+
+  return result;
+};
+
+const parse = (input) => {
+  const source = String(input); // Ensure Buffers are also converted
+  const parsingResult = source.split('\n').map(parseLine); // Split and parse the lines
+
+  return parsingResult;
+};
+
+module.exports = {
+  parse,
+  parseLine,
+};
